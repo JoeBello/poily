ea80af3c9981021381b908e161087dc420920992
Joe Bello <JoeBello82@gmail.com>
2016-10-15 13:26:20 -0400

    Create global config file

---
+++ config.js
@@ -0,0 +1,13 @@
+var config = {};
+
+config.development = {
+  port: 3000,
+  message: 'Running in development mode on port 3000'
+}
+
+config.production = {
+  port: 3000,
+  message: 'Running in production mode on port 3000'
+}
+
+module.exports = config;
