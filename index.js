ea1ce3f20602fb2808794308b8429433a1a70a25
Joe Bello <joebello123@proton.me>
2016-10-15 19:29:47 -0400

    Move server configuration and middleware to seperate files

--- index.js
+++ index.js
@@ -1,68 +1,61 @@
+var config = require('./server/config')[process.env.NODE_ENV];
 require('dotenv').config();
-var express = require('express');
-var config = require('./config')[process.env.NODE_ENV];
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-// **** bodyParser.json(): Parses the text as JSON and exposes the resulting object on req.body. ****
-var places = require('./places/placesRoutes');
-
-var app = express();
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true,
-  sourceMap: true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/places', places)
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname + '/public/index.html'));
-})
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
+var app = require('./server/server');
+// TODO implement a logger
+// var logger = require('./server/util/logger')
 
 app.listen(config.port, function(){
   console.log(config.message);
 })
+
+// var favicon = require('serve-favicon');
+
+// app.use(require('node-sass-middleware')({
+//   src: path.join(__dirname, 'public'),
+//   dest: path.join(__dirname, 'public'),
+//   indentedSyntax: true,
+//   sourceMap: true
+// }));
+// app.use(express.static(path.join(__dirname, 'public')));
+//
+// app.use('/places', places)
+//
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+//
+//
+// app.get('/', function(req, res){
+//   res.sendFile(path.join(__dirname + '/public/index.html'));
+// })
+// // error handlers
+//
+// // development error handler
+// // will print stacktrace
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
+//
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
+//
+//
+// app.listen(config.port, function(){
+//   console.log(config.message);
+// })
