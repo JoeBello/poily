374ea40ceeed498c1acd222361aab1b5f731fc6a
Joe Bello <JoeBello82@gmail.com>
2016-10-15 20:13:07 -0400

    Move middleware into one file

--- server/middleware/appMiddleware.js
+++ server/middleware/appMiddleware.js
@@ -1,7 +1,7 @@
 var morgan = require('morgan');
 var session = require('express-session');
 var bodyParser = require('body-parser');
-// **** bodyParser.json(): Parses the text as JSON and exposes the resulting object on req.body. ****
+// bodyParser.json(): Parses the text as JSON and exposes the resulting object on req.body.
 
 module.exports = function(app){
   app.use(morgan('dev'));
