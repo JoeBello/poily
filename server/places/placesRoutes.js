ecbded442c58a81f30b0665a59c1363a28b4f0d5
Joe Bello <JoeBello82@gmail.com>
2016-10-16 18:10:04 -0400

    Remove duplicate

--- server/places/placesRoutes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var placesRouter = express.Router();
-
-placesRouter.route('/')
-  .get(function(request, response){
-    console.log('request received in places.');
-  });
-
-module.exports = placesRouter;
