c1363bab37cad8442c34eb8cac82e2cd622aea2b
Joe Bello <JoeBello82@gmail.com>
2016-10-22 14:36:25 -0400

    Reorganize dependencies. Rename response variables.

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router();
+
+var bodyParser = require('body-parser'),
+
 var GooglePlacesPromises = require('googleplaces-promises'),
     placesPromise = new GooglePlacesPromises(process.env.GOOGLE_API_KEY);
 
@@ -19,10 +22,10 @@ router.use(function(req, res, next){
 
 router.get('/api/places', function(req, res){
   geocoder.geocode(req.query.location)
-    .then(function parseGeocodeResponse(geoData){
+    .then(function parseGeocodeResponse(geoResponse){
       console.log('Got the geocode data');
       var params = {
-          location: [geoData[0].latitude, geoData[0].longitude],
+          location: [geoResponse[0].latitude, geoResponse[0].longitude],
           types: req.query.venue,
           radius: req.query.radius * 1609.344
           };
@@ -31,9 +34,9 @@ router.get('/api/places', function(req, res){
     .then(function searchPlaces(searchParams){
       return placesPromise.placeSearch(searchParams)
     })
-    .then(function placesResults(results){
-      console.log(results);
-      res.send(results);
+    .then(function placesResults(placesResponse){
+      console.log(placesResponse);
+      res.send(placesResponse);
     })
     .catch(function(err){
       // console.log(err + ' - places router');
