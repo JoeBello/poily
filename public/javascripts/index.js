2b7c821d52ade641075cfefb0da41c60ad683643
Joe Bello <joebello123@proton.me>
2016-10-24 21:48:20 -0400

    Resolve merge conflict.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,25 +1,25 @@
 $(function() {
   var pageToken;
 
-  var ajaxGet = function(url, params, callback, view){
-      $.get(url, params)
-        .done(function(response){
-          console.log('GET success!');
-          console.log(response);
-          pageToken = response.next_page_token || null;
-          callback(response, view);
-        })
-        .fail(function(jqXHR, textStatus){
-          console.log('GET fail!');
-          console.log(textStatus);
-        });
-  }
+  var ajaxGet = function(params){
+    $.get(params.url, params.searchCriteria)
+      .done(function(response){
+        console.log('GET success!');
+        console.log(response);
+        pageToken = response.next_page_token || null;
+        params.callback(response, params.view);
+      })
+      .fail(function(jqXHR, textStatus){
+        console.log('GET fail!');
+        console.log(textStatus);
+      });
+  };
 
   var templateInjector = function(data, view){
     $('main').append(view({
       places: data.results,
     }));
-  }
+  };
 
   $('#placesSearch-submit').click(function(){
     // TODO on initial search, clear main container, insert an empty container
@@ -28,12 +28,25 @@ $(function() {
     $('.placesResults-Container').remove();
     // TODO all query params must be lower case and underscore delimited
     var params = {
+<<<<<<< HEAD
         location: $('#placesSearch-location').val(),
         radius: $('#placesSearch-radius').val(),
         type: $('#placesSearch-venue').val()
         };
     var template = require('../views/placesResults.hbs');
     ajaxGet('http://localhost:3000/api/places', params, templateInjector, template);
+=======
+      url: 'http://localhost:3000/api/places',
+      searchCriteria: {
+          location: $('#placesSearch-location').val(),
+          radius: $('#placesSearch-radius').val(),
+          type: $('#placesSearch-venue').val()
+        },
+      view: require('../views/placesResults.hbs'),
+      callback: templateInjector
+    };
+    ajaxGet(params);
+>>>>>>> develop
   });
 
   $('#more').click(function(){
@@ -41,12 +54,21 @@ $(function() {
     // template
     // TODO all query params must be lower case and underscore delimited
     var params = {
-        location: $('#placesSearch-location').val(),
-        radius: $('#placesSearch-radius').val(),
-        type: $('#placesSearch-venue').val(),
-        pageToken: pageToken
+      url: 'http://localhost:3000/api/places',
+      searchCriteria: {
+          location: $('#placesSearch-location').val(),
+          radius: $('#placesSearch-radius').val(),
+          type: $('#placesSearch-venue').val(),
+          pageToken: pageToken
+        },
+      view: require('../views/placesResults.hbs'),
+      callback: templateInjector
     };
+<<<<<<< HEAD
     var template = require('../views/placesResults.hbs');
     ajaxGet('http://localhost:3000/api/places', params, templateInjector, template);
+=======
+    ajaxGet(params);
+>>>>>>> develop
   });
 })
