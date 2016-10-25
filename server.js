b90be4fb9841a761b473620083cff039ce291581
Joe Bello <JoeBello82@gmail.com>
2016-10-24 21:48:20 -0400

    Resolve merge conflict.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = express();
 var path = require('path');
 var morgan = require('morgan');
 var session = require('express-session');
+var sassMiddleware = require('node-sass-middleware');
 var bodyParser = require('body-parser');
 // **** bodyParser.json(): Parses the text as JSON and exposes the resulting object on req.body. ****
 
@@ -12,11 +13,11 @@ var bodyParser = require('body-parser');
 // var logger = require('./server/util/logger')
 
 
-app.use(require('node-sass-middleware')({
-  src: path.join(__dirname + '/public'),
+app.use(sassMiddleware({
+  src: path.join(__dirname + '/sass'),
   dest: path.join(__dirname + '/public'),
-  indentedSyntax: true,
-  sourceMap: true
+  debug: true,
+  outputStyle: 'compressed'
 }));
 
 app.use(express.static(path.join(__dirname + '/public')));
