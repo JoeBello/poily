f9fa17873f5aaba26afa2b40066b6fb5555d1285
Joe Bello <JoeBello82@gmail.com>
2016-10-29 18:40:33 -0400

    Modify confirmation messages for each configuration.

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -2,17 +2,17 @@ var config = {
   development: {
     env: 'development',
     port: process.env.PORT || 3000,
-    message: 'Running in development mode on port 3000'
+    message: 'Running in development mode on port ' + (process.env.PORT || 3000)
   },
   testing: {
     env: 'testing',
     port: process.env.PORT || 3000,
-    message: 'Running in testing mode on port 3000'
+    message: 'Running in development mode on port ' + (process.env.PORT || 3000)
   },
   production: {
     env: 'production',
     port: process.env.PORT || 3000,
-    message: 'Running in production mode on port 3000'
+    message: 'Running in development mode on port ' + (process.env.PORT || 3000)
   }
 };
 
