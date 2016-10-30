1fa5c53d966c7ca72901accfe865741df9e05108
Joe Bello <JoeBello82@gmail.com>
2016-10-30 15:21:35 -0400

    Create "clear" function to remove last search. Modify naming for pagetoken.

--- public/javascripts/controllers/searchController.js
+++ public/javascripts/controllers/searchController.js
@@ -2,13 +2,30 @@ $(function(){
   // acquire searchView view
   var searchView = require('../views/searchView');
   // receive event properties from main and query RESTful API
+
+  var clear = function(){
+    // remove link for more results
+    $('#getMoreResults').remove();
+
+    // remove old search results
+    $('.placesResults-place').remove();
+
+    // clear previously cached page token
+    localStorage.pagetoken = null;
+
+  };
+
   var start = function(params){
+
+    if (params.newSearch) {
+      // clear everything from previous search
+      clear()
+    }
+
     $.get(params.url, params.queryParams)
       .done(function(response){
-        // if respones yields a pagetoken cache it in browser memory, otherwise
-        // set the pagetoken property to null
-        localStorage.pagetoken = null;
 
+        // if respones yields a pagetoken cache it in browser memory
         if (response.next_page_token) {
           localStorage.pagetoken = response.next_page_token;
         }
