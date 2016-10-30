13f29f1b2d6aaa4e5a42dfdf674aa1cbad576c43
Joe Bello <joebello123@proton.me>
2016-10-30 19:03:27 -0400

    Add regex functionality for underscore delimiting of place search type. Lowercase all query parameters. Modify click handler for more results to accomodate dynamically added link.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,21 +6,33 @@ $(function(){
     // prevent default behavior
     event.preventDefault();
 
+    // TODO input validation
+
+    // cache value for place search type and replace white space with
+    // underscore
+    var placeType = $('#placesSearch-type').val(),
+      placeTypeUnderscored = placeType.replace(/ /g, '_');
+
+    // set value of place search type property to underscore delimited value
+    $('#placesSearch-type').val(placeTypeUnderscored);
+
     // cache form properties
     var form = $(this),
-    // TODO all query params must be lower case and underscore delimited
-    // TODO input validation
-     formProps = {
-      url: form.attr('action'),
-      queryParams: form.serialize(),
-      newSearch: true
-    };
+      formProps = {
+        url: form.attr('action'),
+        queryParams: form.serialize().toLowerCase(),
+        newSearch: true
+      };
+
+    console.log(formProps.queryParams);
 
     // call SearchController start() with form properties
     searchController.start(formProps);
   });
 
-  $('#placesMoreResults').on('click', function(event) {
+  // add event listener to body, to be binded to #getMoreResults link
+  // if/when added
+  $('body').on('click', '#getMoreResults', function(event) {
     // prevent default behavior
     event.preventDefault();
 
