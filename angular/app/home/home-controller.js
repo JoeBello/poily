524b9ffd0b5ed2b523052c1c79427cbed0536961
Joe Bello <JoeBello82@gmail.com>
2016-11-20 14:08:49 -0500

    Move static html from controller to template.

--- angular/app/home/home-controller.js
+++ angular/app/home/home-controller.js
@@ -4,15 +4,4 @@ angular.module('project1')
   .controller('HomeCtrl', function () {
     var home = this;
 
-    home.title = 'Explore your world.';
-
-    home.ctaButton = 'What\'s around?';
-
-    home.aboutLink = 'LEARN MORE ABOUT PROJECT1';
-
-    home.aboutSections = [
-      { 'heading': 'Discover.' },
-      { 'heading': 'Landmark.' },
-      { 'heading': 'Experience.' }
-    ];
   })
