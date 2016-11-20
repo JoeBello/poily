59cd22b5fc94853acef088b73860d67acd08119b
Joe Bello <JoeBello82@gmail.com>
2016-11-19 20:50:27 -0500

    Modify GET route for experimental changes in explore-controller.js.

--- app/routes.js
+++ app/routes.js
@@ -48,6 +48,26 @@ router.get('/api/places', function(req, res){
     })
 });
 
+router.get('/api/userPlaces', function(req, res){
+  console.log('GET request received');
+  console.log(req.query);
+  var params = {
+          location: [req.query.latitude, req.query.longitude],
+          radius: req.query.radius * 1609.344,
+          type: req.query.type,
+          pagetoken: req.query.pagetoken || null
+        };
+    placesPromise.placeSearch(params)
+    .then(function placesResults(placesResponse){
+      console.log('Sending places search response...');
+      res.send(placesResponse);
+    })
+    .catch(function(err){
+      console.log(err.stack);
+      res.status(500).send('Server error!');
+    })
+});
+
 module.exports = function(app){
   app.use('/', router);
 }
