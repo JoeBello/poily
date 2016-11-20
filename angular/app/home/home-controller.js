c56d711d5cd08b9b79f482f70a83a121a24d9b65
Joe Bello <joebello123@proton.me>
2016-11-20 13:36:00 -0500

    Move html from template to controller.

--- angular/app/home/home-controller.js
+++ angular/app/home/home-controller.js
@@ -5,4 +5,14 @@ angular.module('project1')
     var home = this;
 
     home.title = 'Explore your world.';
+
+    home.ctaButton = 'What\'s around?';
+
+    home.aboutLink = 'LEARN MORE ABOUT PROJECT1';
+
+    home.aboutSections = [
+      { 'heading': 'Discover.' },
+      { 'heading': 'Landmark.' },
+      { 'heading': 'Experience.' }
+    ];
   })
