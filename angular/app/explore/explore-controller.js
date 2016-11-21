c5cf46f126339fa15b32d4461893068e797870f9
Joe Bello <JoeBello82@gmail.com>
2016-11-20 19:02:50 -0500

    Remove experimental user location geocoding logic. Add logic to implement PlacesModel service geocoding.

--- angular/app/explore/explore-controller.js
+++ angular/app/explore/explore-controller.js
@@ -4,32 +4,23 @@ angular.module('project1')
   .controller('ExploreCtrl', function (PlacesModel) {
     var explore = this;
 
-    explore.header = 'Where would you like to be?';
-
     explore.userSearch = {
       latitude: '',
       longitude: '',
-      radius: 5,
-      type: 'bank'
+      radius: '',
+      type: ''
     };
 
     explore.results = {};
 
-    // TODO search submission needs to be a service, its used for random
-    // searching upon user arrival, filtered searches, and user searches
+    explore.geo = function () {
+      PlacesModel.defaultSearch()
+      .then(function (result) {
+        console.log('SUCCESS!');
+        console.log(result);
+      });
 
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var coords = position.coords;
-      explore.userSearch.latitude = coords.latitude;
-      explore.userSearch.longitude = coords.longitude;
-      console.log(explore.userLocation);
-      PlacesModel.search(explore.userSearch)
-      .then(function(result) {
-        console.log('SUCCESS!')
-        explore.results = result.results;
-        console.log(explore.results);
 
-      })
-    });
-
+    };
+    
   });
