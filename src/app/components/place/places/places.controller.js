ba7d99856b04089a4bb0d34d2f5464da9f3b5346
Joe Bello <joebello123@proton.me>
2016-12-10 15:57:59 -0500

    Create logic to add place to activities, and to remove place from list.

--- src/app/components/place/places/places.controller.js
+++ src/app/components/place/places/places.controller.js
@@ -1,6 +1,21 @@
 function PlacesController () {
   var ctrl = this;
-  var results = ctrl.places;
+  var places = ctrl.places;
+
+  ctrl.addToAgenda = function (event) {
+    console.log('from place, to places, to console (agenda)...')
+    // TODO add to activities
+  };
+
+  ctrl.remove = function (event) {
+    console.log('removed...');
+    console.log(event.place);
+    for (var i = 0; i < places.length; i++) {
+      if (places[i].name === event.place.name) {
+        places.splice(i, 1);
+      }
+    }
+  }
 }
 
 angular
