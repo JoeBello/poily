5dfa6f2eeaf31c50a67e72660f8be18b12934ce3
Joe Bello <JoeBello82@gmail.com>
2016-12-12 21:01:33 -0500

    Remove remove method. Rename addToAgenda method to addToActivities.

--- src/app/components/place/places/places.controller.js
+++ src/app/components/place/places/places.controller.js
@@ -1,21 +1,13 @@
-function PlacesController () {
+function PlacesController (ActivityService) {
   var ctrl = this;
   var places = ctrl.places;
 
-  ctrl.addToAgenda = function (event) {
+  ctrl.addToActivities = function (event) {
     console.log('from place, to places, to console (agenda)...')
-    // TODO add to activities
+    ActivityService.addActivity(event.place);
   };
 
-  ctrl.remove = function (event) {
-    console.log('removed...');
-    console.log(event.place);
-    for (var i = 0; i < places.length; i++) {
-      if (places[i].name === event.place.name) {
-        places.splice(i, 1);
-      }
-    }
-  }
+
 }
 
 angular
