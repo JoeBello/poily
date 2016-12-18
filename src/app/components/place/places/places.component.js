17d0a432c2fbc4b3bb5fb2d5d902ad3a49f156d5
Joe Bello <joebello123@proton.me>
2016-12-18 13:15:38 -0500

    Move API - places constant to places component.

--- src/app/components/place/places/places.component.js
+++ src/app/components/place/places/places.component.js
@@ -9,6 +9,10 @@ var places = {
 angular
   .module('components.place')
   .component('places', places)
+  .constant('API', {
+    'places': 'http://localhost:3000/api/places?',
+    'userPlaces': 'http://localhost:3000/api/userPlaces?'
+  })
   .config(function ($stateProvider) {
     $stateProvider
       .state('places', {
