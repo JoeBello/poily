2fe35ef9635a70abcb446ac5bd9b1b520a8c5118
Joe Bello <joebello123@proton.me>
2016-12-18 19:47:45 -0500

    Remove console log and test route.

--- server/api/place/place.router.js
+++ server/api/place/place.router.js
@@ -4,15 +4,11 @@ var controller = require('./place.controller');
 
 router.all('*', function (req, res, next) {
   console.log('Request received in places.');
-  console.log(req.query);
   next();
 })
 
 router.route('/')
   .get(controller.get)
 
-router.route('/test')
-  .get(controller.getOne)
 
-
-module.exports = router;
+module.exports = router
