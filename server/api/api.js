4d0bcb5a989b7be9cad655569902e23c292b546e
Joe Bello <joebello123@proton.me>
2016-12-18 19:45:38 -0500

    Remove test route.

--- server/api/api.js
+++ server/api/api.js
@@ -1,7 +1,6 @@
 var router = require('express').Router();
 
 // api router mounts the routers for api resources
-router.use('/hello', require('./activity/activity.router'));
 router.use('/places', require('./place/place.router'));
 
 
