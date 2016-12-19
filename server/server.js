77f167b5c80a1004d1f5904ccdfe760eb1b3f62a
Joe Bello <JoeBello82@gmail.com>
2016-12-18 19:48:10 -0500

    Remove unnecessary require statements.

--- server/server.js
+++ server/server.js
@@ -1,21 +1,17 @@
+// TODO implement a logger
+// var logger = require('./server/util/logger')
+// TODO body-parser for post requests
+
 var express = require('express');
 var app = express();
 var api = require('./api/api');
 var source = express.static('src');
-var morgan = require('morgan');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-// **** bodyParser.json(): Parses the text as JSON and exposes the resulting object on req.body. ****
-
-// TODO implement a logger
-// var logger = require('./server/util/logger')
 
 // setup the api
 app.use('/api', api);
 
 // serve application and pass routing off to AngularJS and UI-Router
 app.use(source);
-
 
 // global error handling
 app.use(function(req, res, next){
