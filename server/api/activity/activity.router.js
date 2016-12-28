f74280636154e565749fd8065c9c04072d567805
Joe Bello <JoeBello82@gmail.com>
2016-12-27 21:41:22 -0500

    Add comments for future routing.

--- server/api/activity/activity.router.js
+++ server/api/activity/activity.router.js
@@ -1,12 +1,17 @@
 var router = require('express').Router();
 var controller = require('./activity.controller');
 
+router.param('')
+
+
 router.route('/')
   .get(controller.get)
   .post(controller.post)
 
 router.route('/:id')
   .get(controller.getOne)
+  // .put
+  // .delete
 
 
 module.exports = router;
