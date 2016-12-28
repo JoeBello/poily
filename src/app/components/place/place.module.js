63dc2d5bd29c8ee0e73dac704971395e3bb49c7b
Joe Bello <JoeBello82@gmail.com>
2016-12-27 21:46:25 -0500

    Relocate API constant to module level.

--- src/app/components/place/place.module.js
+++ src/app/components/place/place.module.js
@@ -1,4 +1,8 @@
 angular
   .module('components.place', [
     'ui.router'
-  ]);
+  ])
+  .constant('API', {
+    'places': 'http://localhost:3000/api/places?',
+    'userPlaces': 'http://localhost:3000/api/userPlaces?'
+  });
