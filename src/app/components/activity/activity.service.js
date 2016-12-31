c2224f0f262424c074de52e6b66186c16800c347
Joe Bello <JoeBello82@gmail.com>
2016-12-30 21:27:33 -0500

    Add clearList() and removeActivity() services.

--- src/app/components/activity/activity.service.js
+++ src/app/components/activity/activity.service.js
@@ -1,42 +1,53 @@
-function ActivityService ($localStorage) {
-  var storage;
+  function ActivityService ($localStorage) {
+    var storage;
 
-  if ($localStorage.activities) {
-    storage = $localStorage.activities;
-  } else {
-    storage = $localStorage.activities = [];
-  }
+    if ($localStorage.activities) {
+      storage = $localStorage.activities;
+    } else {
+      storage = $localStorage.activities = [];
+    }
 
-  function showActivities () {
-    for (var i = 0; i < storage.length; i++) {
-        showActivity(i);
+    function showActivities () {
+      for (var i = 0; i < storage.length; i++) {
+          showActivity(i);
+      }
+    }
+
+    function showActivity(num) {
+      console.log(storage[num]);
+    }
+
+    function removeActivity(activity) {
+      for (var i = 0; i < storage.length; i++) {
+        if (storage[i].name === activity.name) {
+          storage.splice(i, 1);
+        }
+      }
+    }
+
+    function clearStorage() {
+      $localStorage.$reset();
+    }
+
+    return {
+      getActivityList: function () {
+        return storage;
+      },
+      addActivity: function (activity) {
+        storage.push(activity);
+      },
+      removeActivity: function (activity) {
+        return removeActivity(activity);
+      },
+      listActivities: function () {
+        return showActivities();
+      },
+      clearList: function () {
+        return clearStorage();
+      }
     }
   }
 
-  function showActivity(num) {
-    console.log(storage[num]);
-  }
-
-  function clearStorage() {
-    $localStorage.$reset();
-  }
-
-  return {
-    getActivityList: function () {
-      return storage;
-    },
-    addActivity: function (activity) {
-      storage.push(activity);
-    },
-    listActivities: function () {
-      return showActivities();
-    },
-    clearList: function () {
-      return clearStorage();
-    }
-  }
-}
-
-angular
-  .module('components.activity')
-  .service('ActivityService', ActivityService);
+  angular
+    .module('components.activity')
+    .service('ActivityService', ActivityService);
