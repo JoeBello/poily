4e6b2ac5430fc9555cd340cd3d9a2acdf762edcc
Joe Bello <joebello123@proton.me>
2017-01-07 13:06:01 -0500

    Modify API constant to support api uri change.

--- src/app/components/place/place.module.js
+++ src/app/components/place/place.module.js
@@ -2,7 +2,4 @@ angular
   .module('components.place', [
     'ui.router'
   ])
-  .constant('API', {
-    'places': 'http://localhost:3000/api/places?',
-    'userPlaces': 'http://localhost:3000/api/userPlaces?'
-  });
+  .constant('placesAPI', 'http://localhost:3000/api/places?');
