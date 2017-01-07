49c12a9e954ace5a3198a1e9caefb6a1c80ee9c0
Joe Bello <JoeBello82@gmail.com>
2017-01-07 13:05:18 -0500

    Add dotenv. Add and implement morgan.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
-// TODO implement a logger
-// var logger = require('./server/util/logger')
-
+require('dotenv').config();
 var express = require('express');
 var app = express();
+var morgan = require('morgan');
 var api = require('./api/api');
 var source = express.static('src');
 // var mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/project1');
+
+// logging
+app.use(morgan('combined'));
 
 // serve application and pass routing off to AngularJS and UI-Router
 app.use(source);
