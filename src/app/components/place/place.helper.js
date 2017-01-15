1ea338417fdfc9e93a525d8e0f6392ad6caee966
Joe Bello <joebello123@proton.me>
2017-01-15 18:46:55 -0500

    Create helper factory.

---
+++ src/app/components/place/place.helper.js
@@ -0,0 +1,25 @@
+function PlaceHelper() {
+
+  function zeroPad(num, length) {
+      num = num || '';
+      while (num.length < length) {
+        num = "0".concat(num);
+        zeroPad(num, length - 1);
+      }
+      return num;
+  }
+
+  function zipGen() {
+    var generator = Math.floor(Math.random() * 100000).toString();
+    var zip = zeroPad(generator, 5);
+    return zip;
+  }
+
+  return {
+    zipGen: zipGen
+  };
+}
+
+angular
+  .module('components.place')
+  .factory('PlaceHelper', PlaceHelper);
