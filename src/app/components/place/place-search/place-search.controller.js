d9a76d3c225acbfe25d2111e3ea1ce27f55b1e28
Joe Bello <joebello123@proton.me>
2017-01-15 14:14:05 -0500

    Modify type attribute of state params.

--- src/app/components/place/place-search/place-search.controller.js
+++ src/app/components/place/place-search/place-search.controller.js
@@ -1,15 +1,26 @@
 function PlaceSearchController ($state) {
   var ctrl = this;
 
+
   ctrl.searchPlaces = function(event) {
     var criteria = event.criteria;
-    
     $state.go('places', {
       zipcode: criteria.zipcode,
       radius: criteria.radius,
-      type: criteria.type
+      type: criteria.type.type
     });
   };
+
+
+  ctrl.typeOptions = [
+      {type: ' ', value: 'Let\'s see it all !'},
+      {type: 'amusement_park', value: 'Amusement Park'},
+      {type: 'art_gallery', value: 'Art Gallery'},
+      {type: 'bank', value: 'Bank'},
+      {type: 'campground', value: 'Campground'},
+      {type: 'movie_theater', value: 'Movie Theater'},
+      {type: 'cafe', value: 'Cafe'}
+  ];
 
 }
 
