74343cd8c4b426a1edfaaa24850b69ae794c0b2b
Joe Bello <joebello123@proton.me>
2017-01-14 19:12:41 -0500

    Remove unnecessary dependencies, console logs, and comments. Modify event handling to accomodate refactor of ActivityService.

--- src/app/components/place/places/places.controller.js
+++ src/app/components/place/places/places.controller.js
@@ -1,24 +1,21 @@
-function PlacesController (ActivityService, $state, $stateParams, $scope, PlaceService) {
+function PlacesController (ActivityService, $state) {
+  // TODO $onInit
   var ctrl = this,
       places = ctrl.places;
 
   if (places.error) {
-    console.log(places.error);
-    // state.go with params - send message to return to user about redirect
-    $state.go('search');
+    $state.go('search', {error: places.error});
   }
 
-
   ctrl.addActivity = function (event) {
-    // TODO $onInit
-    var place = event.place;
-    var activity = {
-      name: place.name,
-      location: place.vicinity,
-      id: place.place_id
-    };
-    // ActivityService.addActivity(activity);
-    // ActivityService.listActivities();
+    var place = event.place,
+        activity = {
+          name: place.name,
+          location: place.vicinity,
+          id: place.place_id
+        };
+    
+    ActivityService.saveActivity(activity);
   };
 }
 
