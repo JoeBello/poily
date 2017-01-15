13692fa84bfbc374a27c0909774e635f591123c3
Joe Bello <joebello123@proton.me>
2017-01-15 18:47:13 -0500

    Create place-filter component.

---
+++ src/app/components/place/place-filter/place-filter.component.js
@@ -0,0 +1,8 @@
+var placeFilter = {
+  templateUrl: 'app/components/place/place-filter/place-filter.html',
+  controller: 'PlaceFilterController'
+};
+
+angular
+  .module('components.place')
+  .component('placeFilter', placeFilter);
