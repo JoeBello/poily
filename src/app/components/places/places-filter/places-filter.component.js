10a58de67870cf09317f8bfee987aa3f4de63f6f
Joe Bello <JoeBello82@gmail.com>
2017-01-21 18:33:42 -0500

    Move places filters into single component.

--- src/app/components/places/places-filter/places-filter.component.js
+++
@@ -1,8 +0,0 @@
-var placesFilter = {
-  templateUrl: 'app/components/places/places-filter/places-filter.html',
-  controller: 'PlacesFilterController'
-};
-
-angular
-  .module('components.places')
-  .component('placesFilter', placesFilter);
