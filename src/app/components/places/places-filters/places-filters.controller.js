2138267cd42a3e527802aae53255956cac95c491
Joe Bello <JoeBello82@gmail.com>
2017-01-22 18:17:44 -0500

    Add $onInit hook.

--- src/app/components/places/places-filters/places-filters.controller.js
+++ src/app/components/places/places-filters/places-filters.controller.js
@@ -1,53 +1,57 @@
 function PlacesFiltersController ($localStorage) {
-  var ctrl = this,
-      storage = $localStorage.project1.places,
-      zipcode = storage.lastSearch.zipcode || '',
-      lat = storage.lastCoordinates.lat || '',
-      lng = storage.lastCoordinates.lng || '',
-      radius = 10;
+  var ctrl = this;
 
-  ctrl.placesFilters = [
-  {
-    name: 'ATM',
-    zipcode: zipcode || '',
-    lat: lat || '',
-    lng: lng || '',
-    radius: radius,
-    type: 'atm'
-  },
-  {
-    name: 'Hospital',
-    zipcode: zipcode || '',
-    lat: lat || '',
-    lng: lng || '',
-    radius: radius,
-    type: 'hospital'
-  },
-  {
-    name: 'Taxi',
-    zipcode: zipcode || '',
-    lat: lat || '',
-    lng: lng || '',
-    radius: radius,
-    type: 'taxi_stand'
-  },
-  {
-    name: 'Train',
-    zipcode: zipcode || '',
-    lat: lat || '',
-    lng: lng || '',
-    radius: radius,
-    type: 'train_station'
-  },
-  {
-    name: 'Subway',
-    zipcode: zipcode || '',
-    lat: lat || '',
-    lng: lng || '',
-    radius: radius,
-    type: 'subway_station'
+  ctrl.$onInit = function () {
+    var lastSearch = $localStorage.project1.places.lastSearch,
+        zipcode = lastSearch.zipcode || '',
+        latitude = lastSearch.latitude || '',
+        longitude = lastSearch.longitude || '',
+        radius = lastSearch.radius || 10;
+
+    ctrl.placesFilters = [
+      {
+        name: 'ATM',
+        zipcode: zipcode || '',
+        latitude: latitude || '',
+        longitude: longitude || '',
+        radius: radius,
+        type: 'atm'
+      },
+      {
+        name: 'Hospital',
+        zipcode: zipcode || '',
+        latitude: latitude || '',
+        longitude: longitude || '',
+        radius: radius,
+        type: 'hospital'
+      },
+      {
+        name: 'Taxi',
+        zipcode: zipcode || '',
+        latitude: latitude || '',
+        longitude: longitude || '',
+        radius: radius,
+        type: 'taxi_stand'
+      },
+      {
+        name: 'Train',
+        zipcode: zipcode || '',
+        latitude: latitude || '',
+        longitude: longitude || '',
+        radius: radius,
+        type: 'train_station'
+      },
+      {
+        name: 'Subway',
+        zipcode: zipcode || '',
+        latitude: latitude || '',
+        longitude: longitude || '',
+        radius: radius,
+        type: 'subway_station'
+      }
+    ];
   }
-];
+
 
 }
 
