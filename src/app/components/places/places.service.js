b6a6da011bfeea9c623b24862a4c1b98334dbc63
Joe Bello <JoeBello82@gmail.com>
2017-01-22 18:20:16 -0500

    Remove excess variable creation/value caching. Add return statements.

--- src/app/components/places/places.service.js
+++ src/app/components/places/places.service.js
@@ -1,45 +1,33 @@
 function PlacesService ($http, $httpParamSerializer, $localStorage, API,
   PlacesServiceGeocoder) {
 
-  var storage = $localStorage.project1.places;
+  var storage = $localStorage.project1.places,
+      endpoint = API['places'];
 
   if (PlacesServiceGeocoder.supported === true) {
-    geocoder = PlacesServiceGeocoder.geocoder;
+    var geocoder = PlacesServiceGeocoder.geocoder;
   }
 
-  function getUrl(searchParams, type) {
-    var endpoint = API[type],
-        params = $httpParamSerializer(searchParams);
-    return  endpoint.concat(params);
-  }
-
-  function savePosition(coordinates) {
-    return storage.lastCoordinates = coordinates;
+  function buildUrl(searchParams, type) {
+    return  endpoint.concat($httpParamSerializer(searchParams));
   }
 
   function extractResults(responseObject) {
-    console.log(responseObject);
-    // TODO try-catch
-    var results = responseObject.data.results,
-        coordinates = angular.copy(results[0].geometry.location);
-
-    savePosition(coordinates);
+    storage.lastSearch.pageToken = responseObject.data.next_page_token
     return responseObject.data.results;
   }
 
   function geolocatePlaces() {
     if (geocoder === undefined) {
-      var errorObject = { error: 'geolocation service unavailable' };
-      return errorObject;
+      return { error: 'geolocation service unavailable' };
     }
 
     return geocoder()
       .then(function(geocoderResponse) {
-        var geoObject = {
-              lat: geocoderResponse.position.coords.latitude,
-              lng: geocoderResponse.position.coords.longitude
-            };
-        return searchPlaces(geoObject);
+        return searchPlaces({
+              latitude: geocoderResponse.position.coords.latitude,
+              longitude: geocoderResponse.position.coords.longitude
+            });
       })
       .catch(function(error) {
         return error;
@@ -47,15 +35,18 @@ function PlacesService ($http, $httpParamSerializer, $localStorage, API,
   }
 
   function searchPlaces(searchParams) {
-    var url = getUrl({
-      latitude: searchParams.lat || '',
-      longitude: searchParams.lng || '',
+    var searchDetails = {
+      pageToken: searchParams.pageToken || '',
+      latitude: searchParams.latitude || '',
+      longitude: searchParams.longitude || '',
       zipcode: searchParams.zipcode || '',
       radius: searchParams.radius || '',
       type: searchParams.type || ''
-    }, 'places');
+    };
 
-    return $http.get(url)
+    storage.lastSearch = searchDetails;
+
+    return $http.get(buildUrl(searchDetails))
       .then(function (placesResponse) {
         return extractResults(placesResponse);
       })
