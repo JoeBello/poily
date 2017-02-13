4ec405a06e77a0d2664c20486d9b97edd4687ef9
Joe Bello <joebello123@proton.me>
2017-02-13 18:47:59 -0500

    Require app css in bootstrap file.

--- src/app/bootstrap.js
+++ src/app/bootstrap.js
@@ -1,6 +1,8 @@
+require('../css/project1.css');
+
 angular
   .element(document)
   .ready(function() {
     angular.bootstrap(document, [require('./root.module')])
-    console.log('bootstrapping application...');
+    console.log('bootstrapping application...')
   });
