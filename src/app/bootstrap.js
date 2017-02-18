a4c798ccd485c24b28b7bc585616ed77317966bd
Joe Bello <JoeBello82@gmail.com>
2017-02-18 17:40:52 -0500

    Remove console log. Correct eslint error.

--- src/app/bootstrap.js
+++ src/app/bootstrap.js
@@ -3,6 +3,5 @@ require('../css/project1.css');
 angular
   .element(document)
   .ready(function() {
-    angular.bootstrap(document, [require('./root.module')])
-    console.log('bootstrapping application...')
+      angular.bootstrap(document, [require('./root.module')]);
   });
