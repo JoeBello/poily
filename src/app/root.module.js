548ce5b67d43458c0c2c10088a007871f51fc15a
Joe Bello <JoeBello82@gmail.com>
2017-02-18 17:41:25 -0500

    Correct linting error.

--- src/app/root.module.js
+++ src/app/root.module.js
@@ -1,7 +1,7 @@
 var root = angular.module('root', [
     require('./common/app.module.js'),
     require('./components/components.module')
-  ])
+])
   .component('root', require('./root.component'));
 
 module.exports = root.name;
