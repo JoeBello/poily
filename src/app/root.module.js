61d42999f5af87d6f468c14b53b638c0d0e32020
Joe Bello <joebello123@proton.me>
2017-02-19 18:57:42 -0500

    Configure $mdThemingProvider.

--- src/app/root.module.js
+++ src/app/root.module.js
@@ -1,7 +1,12 @@
 var root = angular.module('root', [
+    'ngMaterial',
     require('./common/app.module.js'),
     require('./components/components.module')
 ])
-  .component('root', require('./root.component'));
-
+  .component('root', require('./root.component'))
+  .config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+      .primaryPalette('pink')
+      .accentPalette('orange');
+  });
 module.exports = root.name;
