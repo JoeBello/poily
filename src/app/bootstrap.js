658a75ff02bc034539a1cf64805fbdbc95f727a8
Joe Bello <joebello123@proton.me>
2017-03-12 16:41:15 -0400

    Remove unecessary require statements.

--- src/app/bootstrap.js
+++ src/app/bootstrap.js
@@ -1,7 +1,5 @@
 angular
   .element(document)
   .ready(function() {
-    require('../../node_modules/angular-material/angular-material.min.css');
-    require('../css/project1.css');
     angular.bootstrap(document, [require('./root.module')]);
   });
