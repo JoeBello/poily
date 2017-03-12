84b9db57d21351ba294ba485be0d1ccda53977a5
Joe Bello <joebello123@proton.me>
2017-03-12 16:48:29 -0400

    Remove ngMaterial and related config. Add ngAnimate and ui.bootstrap.

--- src/app/root.module.js
+++ src/app/root.module.js
@@ -1,12 +1,9 @@
 var root = angular.module('root', [
-    'ngMaterial',
+    'ngAnimate',
+    'ui.bootstrap',
     require('./common/app.module.js'),
     require('./components/components.module')
 ])
-  .component('root', require('./root.component'))
-  .config(function($mdThemingProvider) {
-    $mdThemingProvider.theme('default')
-      .primaryPalette('pink')
-      .accentPalette('orange');
-  });
+  .component('root', require('./root.component'));
+
 module.exports = root.name;
