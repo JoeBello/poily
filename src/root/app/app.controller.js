b01e38b31247c7576f52385f4310a6d20012e0e1
Joe Bello <JoeBello82@gmail.com>
2017-03-14 10:16:52 -0400

    Add more types for filter bar object.

--- src/root/app/app.controller.js
+++ src/root/app/app.controller.js
@@ -33,14 +33,41 @@ function AppController() {
       title: 'ATM',
       state: 'places({type: "atm", zipcode: "07065"})'
     },
+    bank: {
+      title: 'Bank',
+      state: 'places({type: "bank", zipcode: 07065})'
+    },
+    bus_station: {
+      title: 'Bus',
+      state: 'places({type: "bus_station", zipcode: 07065})'
+    },
+    gas_station: {
+      title: 'Gas',
+      state: 'places({type: "gas_station", zipcode: 07065})'
+    },
     hospital: {
       title: 'Hospital',
       state: 'places({type: "hospital", zipcode: "07065"})'
     },
+    parking: {
+      title: 'Parking',
+      state: 'places({type: "parking", zipcode: 07065})'
+    },
+    police: {
+      title: 'Police',
+      state: 'places({type: "police", zipcode: 07065})'
+    },
     subway: {
       title: 'Subway',
       state: 'places({type: "subway_station", zipcode: "07065"})'
-
+    },
+    taxi_stand: {
+      title: 'Taxi',
+      state: 'places({type: "taxi_stand", zipcode: 07065})'
+    },
+    train_station: {
+      title: 'Train',
+      state: 'places({type: "train_station", zipcode: 07065})'
     }
   }
 }
