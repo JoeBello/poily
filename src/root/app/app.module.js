5763c8ead70e0af35955a032dad288f811b533f0
Joe Bello <JoeBello82@gmail.com>
2017-03-14 15:47:16 -0400

    Create files for application states.

--- src/root/app/app.module.js
+++ src/root/app/app.module.js
@@ -2,21 +2,11 @@ var common = angular.module('common', [
     'ui.router',
     'ngStorage'
   ])
-  .constant('AppNavConstant', require('./app-nav/app-nav.constant'))
+  .constant('AppConstant', require('./app.constant'))
   .factory('AppStorageService', require('./app-storage.service'))
   .component('app', require('./app.component'))
   .component('appNav', require('./app-nav/app-nav.component'))
   .component('filterBar', require('./app-nav/filter-bar/filter-bar.component'))
-  .config(function($stateProvider, $urlRouterProvider, $urlMatcherFactoryProvider) {
-    $urlRouterProvider.otherwise('/app');
-
-    $urlMatcherFactoryProvider.caseInsensitive(true);
-
-    $stateProvider
-      .state('app', {
-        url: '/app',
-        component: 'app'
-        });
-  });
+  .config(require('./app.state'));
 
 module.exports = common.name;
--- src/root/components/places/places.module.js
+++ src/root/components/places/places.module.js
@@ -8,41 +8,8 @@ var placesComponents = angular.module('components.places', [
   .factory('PlacesService', require('./places.service'))
   .component('places', require('./places/places.component'))
   .component('place', require('./place/place.component'))
-  .config(function($stateProvider) {
-    $stateProvider
-      .state('places', {
-        parent: 'app',
-        url: '/places?zipcode&latitude&longitude&radius&type',
-        component: 'places',
-        resolve: {
-          places: function($stateParams, PlacesService) {
-            if ($stateParams.zipcode || ($stateParams.latitude && $stateParams.longitude))
-            {
-              return PlacesService.searchPlaces($stateParams);
-            } else {
-              return PlacesService.geolocatePlaces();
-            }
-          }
-        }
-      });
-    })
-    .component('placesSearch', require('./places-search/places-search.component'))
-    .component('placesForm', require('./places-form/places-form.component'))
-    .config(function($stateProvider) {
-      $stateProvider
-        .state('search', {
-          parent: 'app',
-          url: '/search',
-          component: 'placesSearch',
-          params: {
-            error: null
-          },
-          resolve: {
-            error: function ($stateParams) {
-              return $stateParams.error;
-            }
-          }
-        });
-    });
+  .component('placesSearch', require('./places-search/places-search.component'))
+  .component('placesForm', require('./places-form/places-form.component'))
+  .config(require('./places.state'));
 
 module.exports = placesComponents.name;
