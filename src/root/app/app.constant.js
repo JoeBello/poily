943d5ff2feffac377392b7dc465d298ac31c04a2
Joe Bello <JoeBello82@gmail.com>
2017-03-19 13:41:21 -0400

    Refactor filter bar and parent components to utilize the location of the user's last search when using filter bar states.

--- src/root/app/app.constant.js
+++ src/root/app/app.constant.js
@@ -29,43 +29,53 @@ module.exports = {
     filters: {
       atm: {
         title: 'ATM',
-        state: 'places({type: "atm", zipcode: "07065"})'
+        state: 'places',
+        type: 'atm'
       },
       bank: {
         title: 'Bank',
-        state: 'places({type: "bank", zipcode: "07065"})'
+        state: 'places',
+        type: 'bank'
       },
       bus_station: {
         title: 'Bus',
-        state: 'places({type: "bus_station", zipcode: "07065"})'
+        state: 'places',
+        type: 'bus_station'
       },
       gas_station: {
         title: 'Gas',
-        state: 'places({type: "gas_station", zipcode: "07065"})'
+        state: 'places',
+        type: 'gas_station'
       },
       hospital: {
         title: 'Hospital',
-        state: 'places({type: "hospital", zipcode: "07065"})'
+        state: 'places',
+        type: 'hospital'
       },
       parking: {
         title: 'Parking',
-        state: 'places({type: "parking", zipcode: "07065"})'
+        state: 'places',
+        type: 'parking'
       },
       police: {
         title: 'Police',
-        state: 'places({type: "police", zipcode: "07065"})'
+        state: 'places',
+        type: 'police'
       },
       subway: {
         title: 'Subway',
-        state: 'places({type: "subway_station", zipcode: "07065"})'
+        state: 'places',
+        type: 'subway_station'
       },
       taxi_stand: {
         title: 'Taxi',
-        state: 'places({type: "taxi_stand", zipcode: "07065"})'
+        state: 'places',
+        type: 'taxi_stand'
       },
       train_station: {
         title: 'Train',
-        state: 'places({type: "train_station", zipcode: "07065"})'
+        state: 'places',
+        type: 'train_station'
       }
     }
   }
