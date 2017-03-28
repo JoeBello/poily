66273e2f4f3749a1132d46048b2b34e49c7bd614
Joe Bello <JoeBello82@gmail.com>
2017-03-27 20:31:10 -0400

    Remove filters constants.

--- src/root/app/app.constant.js
+++ src/root/app/app.constant.js
@@ -24,58 +24,5 @@ module.exports = {
         title: 'Log In'
         // state:
       }
-    },
-
-    filters: {
-      atm: {
-        title: 'ATM',
-        state: 'places',
-        type: 'atm'
-      },
-      bank: {
-        title: 'Bank',
-        state: 'places',
-        type: 'bank'
-      },
-      bus_station: {
-        title: 'Bus',
-        state: 'places',
-        type: 'bus_station'
-      },
-      gas_station: {
-        title: 'Gas',
-        state: 'places',
-        type: 'gas_station'
-      },
-      hospital: {
-        title: 'Hospital',
-        state: 'places',
-        type: 'hospital'
-      },
-      parking: {
-        title: 'Parking',
-        state: 'places',
-        type: 'parking'
-      },
-      police: {
-        title: 'Police',
-        state: 'places',
-        type: 'police'
-      },
-      subway: {
-        title: 'Subway',
-        state: 'places',
-        type: 'subway_station'
-      },
-      taxi_stand: {
-        title: 'Taxi',
-        state: 'places',
-        type: 'taxi_stand'
-      },
-      train_station: {
-        title: 'Train',
-        state: 'places',
-        type: 'train_station'
-      }
     }
   }
