76920e2db1e3e833d0e9b1bf5fb88e8c9e877737
Joe Bello <joebello123@proton.me>
2017-03-27 20:31:57 -0400

    Add filters constants and filters components.

--- src/root/app/app.module.js
+++ src/root/app/app.module.js
@@ -6,7 +6,10 @@ var common = angular.module('common', [
   .factory('AppStorageService', require('./app-storage.service'))
   .component('app', require('./app.component'))
   .component('appNav', require('./app-nav/app-nav.component'))
-  .component('filterBar', require('./app-nav/filter-bar/filter-bar.component'))
+  .constant('FiltersConstant',
+    require('./app-nav/filters/filters.constant'))
+  .component('filters', require('./app-nav/filters/filters/filters.component'))
+  .component('filter', require('./app-nav/filters/filter/filter.component'))
   .config(require('./app.state'));
 
 module.exports = common.name;
