06517ddb1993b88c551b5754e638248c4ad032f9
Joe Bello <JoeBello82@gmail.com>
2017-04-02 13:07:56 -0400

    Enable filter components, and app-nav components to make use of previous search location. Results in not having to geocode user at every attempt to enter "places" state.

--- src/root/app/app-nav/filters/filters/filters.controller.js
+++ src/root/app/app-nav/filters/filters/filters.controller.js
@@ -1,22 +1,26 @@
-function FiltersController(FiltersConstant, AppStorageService, $state) {
+function FiltersController(FiltersConstant, AppStorageService, $state, $scope) {
   var ctrl = this;
 
   ctrl.$onInit = function() {
     angular.forEach(FiltersConstant, function(value, constant) {
       ctrl[constant] = FiltersConstant[constant];
     });
+
+    ctrl.lastLocation = AppStorageService.getLastLocation();
   }
 
-  ctrl.filterResults = function(event) {
-    var lastLocation = AppStorageService.getLastLocation(),
-        stateParams = {
-          radius: 5,
-          type: event.filter.type
-        };
+  $scope.$on('location_change', function(event, location) {
+    ctrl.lastLocation = location;
+  })
 
-    if (lastLocation !== null) {
-      stateParams.location = lastLocation;
-    }
+  ctrl.filterResults = function(event) {
+    var stateParams = {
+      radius: 5,
+      type: event.filter.type
+    };
+
+    stateParams.location = ctrl.lastLocation;
+
 
     $state.go(event.filter.state, stateParams);
   }
