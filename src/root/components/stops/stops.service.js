d78afbc78a12694e9fdb46eb23528100b5090f4d
Joe Bello <JoeBello82@gmail.com>
2017-04-04 18:03:35 -0400

    Fix bug with stops counter.

--- src/root/components/stops/stops.service.js
+++ src/root/components/stops/stops.service.js
@@ -4,7 +4,7 @@ function StopsService ($localStorage, $rootScope) {
     saveStop: function(stop) {
       var stops = $localStorage.project1.stops;
       stops.push(stop);
-      return $rootScope.$broadcast('stop_change', stops);
+      return $rootScope.$broadcast('stop_change', stops.length);
     },
     // retrieve all stops
     getStops: function() {
@@ -15,13 +15,13 @@ function StopsService ($localStorage, $rootScope) {
       var stops = $localStorage.project1.stops;
 
       stops.splice(stops.indexOf(stop), 1);
-      return $rootScope.$broadcast('stop_change', stop);
+      return $rootScope.$broadcast('stop_change', stops.length);
     },
     // remove all stops
     destroyStops: function() {
       var stops = $localStorage.project1.stops;
       stops.splice(0, stops.length);
-      return $rootScope.$broadcast('stop_change', stops);
+      return $rootScope.$broadcast('stop_change', stops.length);
     }
   };
 }
