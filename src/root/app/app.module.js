d5951092fe0c5dd231deb7bfd8eca29260bec74b
Joe Bello <JoeBello82@gmail.com>
2017-04-17 16:32:27 -0400

    Move filters (presets) to component module.

--- src/root/app/app.module.js
+++ src/root/app/app.module.js
@@ -7,10 +7,6 @@ var common = angular.module('common', [
   .component('app', require('./app.component'))
   .component('appNav', require('./app-nav/app-nav.component'))
   .component('appScroll', require('./app-scroll/app-scroll.component'))
-  .constant('FiltersConstant',
-    require('./app-nav/filters/filters.constant'))
-  .component('filters', require('./app-nav/filters/filters/filters.component'))
-  .component('filter', require('./app-nav/filters/filter/filter.component'))
   .config(require('./app.state'));
 
 module.exports = common.name;
