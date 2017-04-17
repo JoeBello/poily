5a630227a6ca13bc943e5c8860f04643429d32af
Joe Bello <joebello123@proton.me>
2017-04-17 16:57:34 -0400

    Typo: "search" component changed to "location".

--- src/root/components/location/location.module.js
+++ src/root/components/location/location.module.js
@@ -1,6 +1,6 @@
 var locationComponents = angular.module('components.location', [])
   .factory('LocationFactory', require('./location.factory'))
   .component('locationDetail', require('./location-detail/location-detail.component'))
-  .component('search', require('./search/search.component'));
+  .component('location', require('./location/location.component'));
 
 module.exports = locationComponents.name;
