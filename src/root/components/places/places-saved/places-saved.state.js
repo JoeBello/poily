a1a8bb50818eb707026f183f96b64d143cab12ac
Joe Bello <joebello123@proton.me>
2017-04-24 17:22:08 -0400

    No major changes.

--- src/root/components/places/places-saved/places-saved.state.js
+++ src/root/components/places/places-saved/places-saved.state.js
@@ -3,14 +3,14 @@ function placesSavedState($stateProvider) {
     .state('saved', {
       parent: 'app',
       url: '/saved',
-      resolve: {
-        places: function(PlacesFactory) {
-          return PlacesFactory.getSavedPlaces();
-        }
-      },
       views: {
         main: {
           component: 'placesSaved',
+        }
+      },
+      resolve: {
+        places: function(PlacesFactory) {
+          return PlacesFactory.getSavedPlaces();
         }
       }
     });
