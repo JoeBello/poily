4a6fb3119a60770f8cced45470c25d50ca3bd25a
Joe Bello <joebello123@proton.me>
2017-04-24 17:20:27 -0400

    Replace $stateParams with $transition$.params().

--- src/root/components/places/places/places.state.js
+++ src/root/components/places/places/places.state.js
@@ -3,23 +3,26 @@ function placesState($stateProvider) {
     .state('places', {
       parent: 'app',
       url: '/places?location&radius&type',
-      resolve: {
-        places: function($stateParams, LocationFactory, PlacesFactory) {
-          if (!$stateParams.location) {
-            return LocationFactory.geolocate()
-            .then(function(coordinates) {
-              var searchParams = angular.copy($stateParams);
-              searchParams.location = coordinates;
-              return PlacesFactory.searchNewPlaces(searchParams);
-            })
-          }
-
-          return PlacesFactory.searchNewPlaces($stateParams);
-        }
-      },
       views: {
         main: {
           component: 'places',
+        }
+      },
+      resolve: {
+        places: function($transition$, LocationFactory, PlacesFactory) {
+          if (!$transition$.params().location) {
+            return LocationFactory.geolocate()
+            .then(function(coordinates) {
+              var searchParams = angular.copy($transition$.params());
+              searchParams.location = coordinates;
+              return PlacesFactory.searchNewPlaces(searchParams);
+            })
+            .catch(function(error) {
+              return error;
+            });
+          }
+
+          return PlacesFactory.searchNewPlaces($transition$.params());
         }
       }
     });
