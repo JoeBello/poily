9f15d3dd8449c5f74f801d1ed26e990a65ed026c
Joe Bello <JoeBello82@gmail.com>
2017-05-01 20:32:56 -0400

    Modify config to implement certain configurations based on environment flag.

--- webpack.config.js
+++ webpack.config.js
@@ -2,73 +2,80 @@ var path = require('path');
 var app = path.resolve(__dirname, 'src', 'root');
 var webpack = require('webpack');
 var ngAnnotateWebpackPlugin = require('ng-annotate-webpack-plugin');
-var progressBarPlugin = require('progress-bar-webpack-plugin');
 var extractTextPlugin = require('extract-text-webpack-plugin');
-
+var progressBarPlugin = require('progress-bar-webpack-plugin');
 
 module.exports = function(env) {
-  return {
-  context: path.resolve(__dirname, 'src'),
-  entry: {
-    vendors: [
-      'angular',
-      'angular-animate',
-      'angular-ui-bootstrap',
-      'angular-ui-router',
-      'ngstorage-webpack'
-    ],
-    app: path.resolve(app, 'bootstrap')
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'src', 'dist'),
-    publicPath: '/dist/'
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'src'),
-    port: 3001,
-    publicPath: '/dist/',
-    proxy: {
-      '/api/*': 'http://localhost:3000'
-    }
-  },
-  devtool: env === 'prod' ? 'source-map' : 'eval',
-  module: {
-    rules: [
-      {test: /\.html$/, use: ['html-loader']},
-      {test: /\.css$/,
-        use: extractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
-        })
-      },
-      {test: /\.scss$/,
-        use: extractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
-      },
-      {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: "url-loader?limit=10000&mimetype=application/font-woff"
-      },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: "file-loader"
-      }
+  var config =  {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+      vendors: [
+        'angular',
+        'angular-animate',
+        'angular-ui-bootstrap',
+        'angular-ui-router',
+        'ngstorage-webpack'
+      ],
+      app: path.resolve(app, 'bootstrap')
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'src', 'dist'),
+      publicPath: '/dist/',
+      sourceMapFilename: '[name].map'
+    },
+    module: {
+      rules: [
+        {test: /\.html$/, use: ['html-loader']},
+        {test: /\.css$/,
+          use: extractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: 'css-loader'
+          })
+        },
+        {test: /\.scss$/,
+          use: extractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader']
+          })
+        },
+        {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          use: "url-loader?limit=10000&mimetype=application/font-woff"
+        },
+        { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          use: "file-loader"
+        }
+      ]
+    },
+    plugins: [
+      new progressBarPlugin(),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendors'
+      }),
+      new ngAnnotateWebpackPlugin({
+        add: true
+      }),
+      new extractTextPlugin('styles.css'),
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: false,
+        mangle: false
+      })
     ]
-  },
-  plugins: [
-    new progressBarPlugin(),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendors'
-    }),
-    new ngAnnotateWebpackPlugin({
-      add: true
-    }),
-    new extractTextPlugin('styles.css'),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: false,
-      mangle: false
-    })
-  ]
+  };
+
+  if (env === 'dev') {
+    config.devtool = 'eval';
+    config.devServer = {
+        contentBase: path.resolve(__dirname, 'src'),
+        port: 3001,
+        publicPath: '/dist/',
+        proxy: { '/api/*': 'http://localhost:3000' }
+      };
   }
+
+  if (env === 'prod') {
+    config.devtool = 'source-map';
+  }
+
+  return config;
 };
