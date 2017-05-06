a4d9a0cd7126a63ed5b50ed4172383a5834c2545
Joe Bello <joebello123@proton.me>
2017-05-06 16:45:29 -0400

    Add helmet and hpp to project.

--- server/server.js
+++ server/server.js
@@ -5,22 +5,28 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var api = require('./api/api');
 var source = express.static('src');
+var helmet = require('helmet');
+var hpp = require('hpp');
 
-// logging
+app.use(helmet());
+
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"]
+  }
+}));
+
 app.use(morgan('combined'));
 
-// parse POST request
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// serve application and pass routing off to AngularJS and UI-Router
+app.use(hpp());
+
 app.use(source);
 
-// setup the api
 app.use('/api', api);
 
-
-// global error handling
 app.use(function(req, res, next){
   var err = new Error('Not found');
   err.status = 404;
