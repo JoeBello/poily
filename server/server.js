bd5f478a74f49bc4e7d5e4bc75abe40242111a44
Joe Bello <joebello123@proton.me>
2017-05-06 18:32:00 -0400

    Restructure server configuration to seperate dev and prod configurations into individual files.

--- server/server.js
+++ server/server.js
@@ -1,27 +1,28 @@
-require('dotenv').config();
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
-var api = require('./api/api');
-var source = express.static('src');
-var helmet = require('helmet');
-var hpp = require('hpp');
+var express = require('express'),
+    api = require('./api/api'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    config = require('./config/config'),
+    helmet = require('helmet'),
+    hpp = require('hpp'),
+    morgan = require('morgan'),
+    noop = function(){},
+    source = express.static('src')
 
-app.use(helmet());
+config.env.prod ? app.use(helmet()) : noop();
 
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"]
-  }
-}));
+config.env.prod ? app.use(helmet.contentSecurityPolicy({
+                            directives: {
+                              defaultSrc: ["'self'"]
+                            }
+                          })) : noop();
 
-app.use(morgan('combined'));
+config.logging ? app.use(morgan('combined')) : noop();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(hpp());
+config.env.prod ? app.use(hpp()) : noop();
 
 app.use(source);
 
