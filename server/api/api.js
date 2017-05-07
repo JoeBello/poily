8ca209221f66bae8339775bd3136bf5c1bf78c18
Joe Bello <JoeBello82@gmail.com>
2017-05-07 14:24:46 -0400

    Chore

--- server/api/api.js
+++ server/api/api.js
@@ -1,6 +1,5 @@
 var router = require('express').Router();
 
-// api router mounts the routers for api resources
 router.use('/places', require('./places/places.router'));
 
 module.exports = router;
