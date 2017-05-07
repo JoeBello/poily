020ec96c235fae8016b83cb49f49d520b68db093
Joe Bello <JoeBello82@gmail.com>
2017-05-07 14:25:42 -0400

    Handle instance of a query without a location, via a rejected promise.

--- server/api/places/places.controller.js
+++ server/api/places/places.controller.js
@@ -1,9 +1,11 @@
 var placesModel = require('./places.model');
 
-
 exports.getPlaces = function(req, res, next) {
   placesModel.getPlaces(req.query)
   .then(function(places){
     res.send(places);
   })
+  .catch(function(error) {
+    next(error);
+  });
 };
