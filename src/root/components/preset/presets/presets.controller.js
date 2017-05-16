b46585e8253240d2161de839c063bd0c2bb91040
Joe Bello <joebello123@proton.me>
2017-05-16 16:57:54 -0400

    Refactor: Modify name of constant handling function.

--- src/root/components/preset/presets/presets.controller.js
+++ src/root/components/preset/presets/presets.controller.js
@@ -2,16 +2,16 @@ function PresetsController(PresetConstant, AppStorageService, $scope, $state) {
   var ctrl = this;
 
   ctrl.$onInit = function onInit() {
-    angular.forEach(PresetConstant, function(value, constant) {
-      ctrl[constant] = PresetConstant[constant];
-    });
+    angular.forEach(PresetConstant,
+      function eachPresetConstant(value, constant) {
+        ctrl[constant] = PresetConstant[constant];
+      });
   }
 
   ctrl.presetSearch = function presetSearch(event) {
     var lastLocation = AppStorageService.getLastLocation(),
         stateParams = {
           location: lastLocation,
-          radius: 5,
           type: event.preset.type
         };
 
